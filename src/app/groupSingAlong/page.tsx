a84336246e283a30a8dcd8828c7f1fbb38204ac9
Erin Kerr <erin.kerr17@gmail.com>
2025-01-18 18:24:06 -0800

    link to individual project pages

--- src/app/groupSingAlong/page.tsx
+++ src/app/groupSingAlong/page.tsx
@@ -3,14 +3,16 @@
 import DarkModeToggle from "@/components/DarkModeToggle";
 import { poiretOne } from "../../../public/fonts/fonts";
 
-export default function Carpoolio() {
+export default function GroupSingAlong() {
   return (
     <div className=" bg-slate-100 dark:bg-slate-900 text-white">
       <h1
         className={`${poiretOne.className} flex justify-center items-center font-medium text-md md:text-lg lg:text-4xl p-2 sm:p-3 md:p-6 lg:p-8`}
       >
-        Carpoolio
+        Group Sing Along
       </h1>
+      <h2>Subtitle </h2>
+      {/* <p>I created this app for my stepdad </p> */}
 
       <DarkModeToggle />
     </div>

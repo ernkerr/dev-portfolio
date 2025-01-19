723cc11cf39d9a57515f7534511244cdbbe7aca4
Erin Kerr <erin.kerr17@gmail.com>
2025-01-18 18:23:45 -0800

    link to project page

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import DarkModeToggle from "@/components/DarkModeToggle";
+import { poiretOne } from "../../../public/fonts/fonts";
+
+export default function Projects() {
+  return (
+    <div className=" bg-slate-100 dark:bg-slate-900 text-white">
+      <h1
+        className={`${poiretOne.className} flex justify-center items-center font-medium text-md md:text-lg lg:text-4xl p-2 sm:p-3 md:p-6 lg:p-8`}
+      >
+        Projects
+      </h1>
+      <h2>Subtitle </h2>
+
+      <DarkModeToggle />
+    </div>
+  );
+}

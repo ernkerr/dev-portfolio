a3755f83fd964c3621f2c2eea5f9ae64aacdb30d
Erin Kerr <erin.kerr17@gmail.com>
2025-08-06 17:55:22 -0700

    Update link text in ProjectTemplate to conditionally display "View App" or "View Site"

--- src/components/ProjectTemplate.tsx
+++ src/components/ProjectTemplate.tsx
@@ -72,7 +72,7 @@ export default function ProjectTemplate({
           <div className="flex flex-col gap-4 text-sm">
             <h3 className="font-bold md:text-lg">Live</h3>
             <Link href={link} className="cursor-pointer text-blue-500">
-              View Site
+              {link.includes("apple") ? "View App" : "View Site"}
             </Link>
           </div>
         </div>

61e07b1101098c45d479d1e801111aabc08bfb36
Erin Kerr <erin.kerr17@gmail.com>
2026-02-02 13:31:38 -0800

    Update Disco component styles for improved image responsiveness and overflow handling

--- src/components/Disco.tsx
+++ src/components/Disco.tsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 export default function Disco() {
   return (
-    <div className="flex h-full w-full items-center justify-center">
+    <div className="flex h-full w-full items-center justify-center overflow-hidden">
       <Image
         src="/disco.gif"
         alt="Disco animation"
         height={100}
         width={100}
-        className="h-20 w-20 object-contain"
+        className="h-8 w-8 object-contain sm:h-10 sm:w-10 md:h-12 md:w-12"
         unoptimized
       />
     </div>
